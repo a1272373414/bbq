767ae48af6f4f07351fff5b7069eb4ec0c73c519
Administrator <Administrator@SD-201610021622>
2016-11-29 16:35:31 +0800

    map

--- src/main/java/com/tango/bbq/controller/FirstController.java
+++ src/main/java/com/tango/bbq/controller/FirstController.java
@@ -1,5 +1,8 @@
 package com.tango.bbq.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +12,11 @@ public class FirstController {
   
   @RequestMapping("/monitor")
   public String home(Model model) {
-//	  Map<String, String> map = new HashMap<String, String>();
+	  Map<String, String> map = new HashMap<String, String>();
 //	  map.put("testKey", "testValue");
-//	  model.addAttribute(map);
 	  model.addAttribute("testkey","testvalue");
+	  model.addAttribute(map);
+//	  model.put("testkey","testvalue");
       return "index";
   }
 }
