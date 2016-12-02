ec6ce084fe767db03d70264a28510f95180be4c2
Administrator <Administrator@SD-201610021622>
2016-12-02 15:18:24 +0800

    echart

--- src/main/java/com/tango/bbq/controller/FirstController.java
+++ src/main/java/com/tango/bbq/controller/FirstController.java
@@ -1,25 +1,22 @@
 package com.tango.bbq.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.tango.bbq.util.CSVUtils;
 
 @Controller
 public class FirstController {
   
   @RequestMapping("/monitor")
   public String home(Model model) {
-	  Map<String, String> map = new HashMap<String, String>();
+//	  Map<String, String> map = new HashMap<String, String>();
 //	  map.put("testKey", "testValue");
 //	  CSVUtils.readeCsv();
 //	  model.addAttribute("testkey","testvalue");
 //	  model.addAttribute(map);
 //	  model.put("testkey","testvalue");
-      return "area-simple";
+	  model.addAttribute("date","2016/11/10-1,2016/11/10-2,2016/11/10-3,2016/11/10-4,2016/11/10-5,2016/11/10-6,2016/11/10-7");
+	  model.addAttribute("data","3000,3600,3204,3640,3690,3900,3100");
+      return "monitorChart";
   }
 }
